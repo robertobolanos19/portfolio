0b6522acf1f69f2216bad8066b8eab95d757dd3e
Roberto Bolanos <roberto_bolanos@icloud.com>
2024-07-11 13:39:57 -0700

    Update projectSchema to match database fields

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // MongoDB connection string
 const mongoUri = process.env.MONGODB_URI; // Use environment variable
@@ -13,11 +13,13 @@ mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // Define a schema for your projects
 const projectSchema = new mongoose.Schema({
-    name: String,
-    category: String,
-    description: String,
-    imageUrl: String,
-    // Add other fields as needed
+    title: { type: String, required: true },
+    class: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    model: { type: String, required: true },
+    year: { type: Number, required: true },
+    software: { type: String, required: true },
 });
 
 // Create a model for your projects
@@ -27,7 +29,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Enable CORS for all routes
-app.use(cors({ origin: 'http://inexplicablejourney.com/' })); // Update this to your frontend URL if different
+app.use(cors({ origin: 'http://yourdomain.com' })); // Replace with your frontend domain
 
 // Serve static files from the 'imgs' directory
 app.use('/imgs', express.static('imgs')); // Adjust the directory name as needed
