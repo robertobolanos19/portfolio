6e857ce9fabcc92624a50791c22ac213cb479d70
Roberto Bolanos <roberto_bolanos@icloud.com>
2024-07-14 00:15:22 -0700

    still fixing

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@
 const config = {
   apiUrl: window.location.hostname === 'localhost'
     ? 'http://localhost:3001'
-    : 'https://portfolio-16-8om3.onrender.com'
+    : 'https://portfolio-bbup.onrender.com'
 };
 
 export default config;
